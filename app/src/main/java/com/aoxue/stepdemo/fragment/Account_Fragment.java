0827c518f96e04b163800b4d612313b96b08163b
jhcax <jhcax@foxmail.com>
2019-06-13 22:04:32 +0800

    UI Account

--- app/src/main/java/com/aoxue/stepdemo/fragment/Account_Fragment.java
+++ app/src/main/java/com/aoxue/stepdemo/fragment/Account_Fragment.java
@@ -9,7 +9,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +21,10 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.aoxue.stepdemo.utils.ConstantUtils;
 import com.aoxue.stepdemo.utils.MyDatabaseHelper;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.aoxue.stepdemo.PlanSetting_Activity;
 import com.aoxue.stepdemo.R;
@@ -26,11 +32,16 @@ import com.aoxue.stepdemo.Settings_Activity;
 import com.aoxue.stepdemo.TuLinTalk_Activity;
 import com.aoxue.stepdemo.WeekRecord_Activity;
 import com.aoxue.stepdemo.utils.PermissionUtils;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
+import java.io.File;
 import java.text.DecimalFormat;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Context.MODE_PRIVATE;
 
 /**
@@ -50,18 +61,22 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
     LinearLayout planSettingLayout;
     LinearLayout weekRecordLayout;
     LinearLayout talkLayout;
+    LinearLayout sloganLayout;
     TextView tvHeight;
     TextView tvWeight;
     TextView tvBMI;
-    TextView settingBtn;
     TextView accountDistance;
+    TextView username;
+    TextView slogan;
 
-    LayoutInflater layoutInflater;
-    View dialogView;
-    TextView unit;
-    EditText editText;
+
+//    LayoutInflater layoutInflater;
+//    View dialogView;
+//    TextView unit;
+//    EditText editText;
 
     SharedPreferences.Editor editor;
+    SharedPreferences.Editor editorSettings;
 
     private CircleImageView icon;
 
@@ -99,8 +114,10 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
         tvWeight = view.findViewById(R.id.tv_weight);
         tvBMI = view.findViewById(R.id.tv_bmi);
         icon = (CircleImageView) view.findViewById(R.id.iv_icon);
-        settingBtn = view.findViewById(R.id.setting_btn);
         accountDistance = view.findViewById(R.id.account_distance);
+        username = view.findViewById(R.id.username);
+        slogan = view.findViewById(R.id.slogan);
+        sloganLayout = view.findViewById(R.id.slogan_layout);
 
         // 设置对应的监听事件
         planSettingLayout.setOnClickListener(this);
@@ -110,12 +127,9 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
         tvWeight.setOnClickListener(this);
         tvBMI.setOnClickListener(this);
         icon.setOnClickListener(this);
-        settingBtn.setOnClickListener(this);
+        username.setOnClickListener(this);
+        sloganLayout.setOnClickListener(this);
 
-        layoutInflater = LayoutInflater.from(getActivity());
-        dialogView = layoutInflater.inflate(R.layout.dialog_parameters, null);
-        unit = (TextView) dialogView.findViewById(R.id.unit);
-        editText = (EditText) dialogView.findViewById(R.id.editText);
 
     }
 
@@ -148,6 +162,18 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
                     .override(300, 300)//指定图片大小
                     .into(icon);
         }
+
+        if (settings.contains("username")) {
+            String user = settings.getString("username", null);
+            username.setText(user);
+        }
+
+        if (settings.contains("slogan")) {
+            String slog = settings.getString("slogan", null);
+            slogan.setText(slog);
+        }
+
+        editorSettings = getActivity().getSharedPreferences("settings", MODE_PRIVATE).edit();
     }
 
     private void initDB() {
@@ -178,6 +204,14 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+        View dialogView = layoutInflater.inflate(R.layout.dialog_parameters, null);
+        TextView unit = (TextView) dialogView.findViewById(R.id.unit);
+        final EditText editText = (EditText) dialogView.findViewById(R.id.editText);
+
+        View dialogView2 = layoutInflater.inflate(R.layout.dialog_no_parameters, null);
+        final EditText editText2 = (EditText) dialogView2.findViewById(R.id.editText2);
+
         switch (view.getId()) {
             case R.id.plan_setting_id:
                 Intent goPlan = new Intent(getContext(), PlanSetting_Activity.class);
@@ -193,6 +227,7 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
                 break;
 
             case R.id.tv_height:
+
                 unit.setText(" cm");
 
                 new AlertDialog.Builder(getActivity())
@@ -257,17 +292,124 @@ public class Account_Fragment extends Fragment implements View.OnClickListener {
                         .show();
                 break;
 
-            case R.id.setting_btn:
-                Intent intent = new Intent(getActivity(),
-                        Settings_Activity.class);
-                startActivity(intent);
+            case R.id.iv_icon:
+                setAvatar();
                 break;
 
-            case R.id.iv_icon:
+            case R.id.username:
+                editText2.setMaxLines(1);
+                editText2.getLayoutParams().width = 250;
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("请设置一个用户名吧!")
+                        .setView(dialogView2)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                editorSettings.putString("username",
+                                        editText2.getText().toString());
+                                editorSettings.apply();
+
+                                getFragmentManager()
+                                        .beginTransaction()
+                                        .replace(R.id.id_nav_table_content, new Account_Fragment())
+                                        .commitNow();
+                            }
+                        })
+                        .setNegativeButton("取消", null)
+                        .show();
+                break;
+
+            case R.id.slogan_layout:
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("请设置一句话的签名吧!")
+                        .setView(dialogView2)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                editorSettings.putString("slogan",
+                                        editText2.getText().toString());
+                                editorSettings.apply();
+
+                                getFragmentManager()
+                                        .beginTransaction()
+                                        .replace(R.id.id_nav_table_content, new Account_Fragment())
+                                        .commitNow();
+                            }
+                        })
+                        .setNegativeButton("取消", null)
+                        .show();
+                break;
 
             default:
                 break;
         }
+    }
+
+    private void setAvatar() {
+        Intent intent1 = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent1.setType("image/*");
+
+        Intent intent2 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File file = new File(Environment.getExternalStorageDirectory(), ConstantUtils.HEAD_PICTURE);
+        Uri uri = Uri.fromFile(file);
+
+        intent2.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        Intent chooser = Intent.createChooser(intent1, "选择头像");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{intent2});
+        startActivityForResult(chooser, 101);// --------------->101
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == 101) {
+                Uri uri = null;
+                if (data != null) {
+                    uri = data.getData();
+                } else {
+                    // 相机拍照
+                    uri = Uri.fromFile(ConstantUtils.bgFile);
+                }
+
+                RequestListener mRequestListener = new RequestListener() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e,
+                                                Object model, Target target, boolean isFirstResource) {
+                        Log.e("Glide", "onException: " + e.toString()
+                                + "  model:" + model + " isFirstResource: " + isFirstResource);
+                        icon.setImageResource(R.mipmap.icon_app);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Object resource, Object model,
+                                                   Target target, DataSource dataSource,
+                                                   boolean isFirstResource) {
+                        Log.e("Glide", "model:" + model + " isFirstResource: " + isFirstResource);
+                        return false;
+                    }
+                };
+
+                if (uri != null) {
+                    editorSettings.putString("portrait", uri.toString());
+                    editorSettings.apply();
+                    Glide.with(this)
+                            .load(uri)
+                            .placeholder(R.mipmap.akkarin)
+                            .error(R.mipmap.icon_app)
+                            .diskCacheStrategy(DiskCacheStrategy.NONE)
+                            .override(300, 300)//指定图片大小
+                            .addListener(mRequestListener)
+                            .into(icon);
+                }
+
+            }
+        }
+
     }
 
 }
